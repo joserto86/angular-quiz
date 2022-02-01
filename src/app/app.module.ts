1e73d199a6d396fcbc84488f709bed1af8d000ea
Jose Alberto Hernán <joserto86@gmail.com>
2022-02-01 22:18:52 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InitComponent } from './components/init/init.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InitComponent
   ],
   imports: [
     BrowserModule,
